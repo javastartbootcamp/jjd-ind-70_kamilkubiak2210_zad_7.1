4e752967e1d78d4e4b5c1e025a73939862a9243a
kamilkubiak2210 <kamil.kubiak4@outlook.com>
2023-04-11 12:00:14 +0200

    Rozwiązanie

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -5,10 +5,19 @@ public class Main {
     public static void main(String[] args) {
         PalindromeService palindromeService = new PalindromeService();
 
-        int[] array = new int[]{1, 2, 3};
+        int[] array = new int[]{1, 2, 3, 2, 1};
+        int[] array2 = new int[]{1, 2, 3, 4};
+        int[] array3 = new int[]{1, 2, 3, 4, 3, 2};
+        int[] array4 = new int[]{};
 
-        boolean isPalindrome = palindromeService.isPalindrome(array);
+        boolean isPalindrome1 = palindromeService.isPalindrome(array);
+        boolean isPalindrome2 = palindromeService.isPalindrome(array2);
+        boolean isPalindrome3 = palindromeService.isPalindrome(array3);
+        boolean isPalindrome4 = palindromeService.isPalindrome(array4);
 
-        System.out.println(isPalindrome);
+        System.out.println(isPalindrome1);
+        System.out.println(isPalindrome2);
+        System.out.println(isPalindrome3);
+        System.out.println(isPalindrome4);
     }
 }
--- src/main/java/pl/javastart/task/PalindromeService.java
+++ src/main/java/pl/javastart/task/PalindromeService.java
@@ -4,6 +4,23 @@ public class PalindromeService {
 
     // nie zmieniaj sygnatury tej metody. Jest ona testowana w PalindromeServiceTest
     public boolean isPalindrome(int[] array) {
-        return false;
+
+        if (array.length == 0) {
+            return false;
+        }
+
+        int number = 0;
+        for (int i : array) {
+            number = number * 10 + i;
+        }
+
+        int originalNumber = number;
+
+        int reversedNumber = 0;
+        while (originalNumber != 0) {
+            reversedNumber = reversedNumber * 10 + originalNumber % 10;
+            originalNumber /= 10;
+        }
+        return number == reversedNumber;
     }
 }
