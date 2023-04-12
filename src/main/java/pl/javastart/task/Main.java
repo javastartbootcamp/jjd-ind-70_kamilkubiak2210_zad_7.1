a48cbb6a67a87c7ccb81ba61c5602bb421229981
kamilkubiak2210 <kamil.kubiak4@outlook.com>
2023-04-12 18:18:45 +0200

    Poprawki2

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,21 +3,23 @@ package pl.javastart.task;
 public class Main {
 
     public static void main(String[] args) {
-        PalindromeService palindromeService = new PalindromeService();
 
         int[] array = new int[]{1, 2, 3, 2, 1};
-        int[] array2 = new int[]{1, 2, 3, 4};
+        int[] array2 = new int[]{1, 2, 3, 1};
         int[] array3 = new int[]{1, 2, 3, 4, 3, 2};
         int[] array4 = null;
+        int[] array5 = new int[]{1, 2, 1};
 
-        boolean isPalindrome1 = palindromeService.isPalindrome(array);
-        boolean isPalindrome2 = palindromeService.isPalindrome(array2);
-        boolean isPalindrome3 = palindromeService.isPalindrome(array3);
-        boolean isPalindrome4 = palindromeService.isPalindrome(array4);
+        boolean isPalindrome1 = PalindromeService.isPalindrome(array);
+        boolean isPalindrome2 = PalindromeService.isPalindrome(array2);
+        boolean isPalindrome3 = PalindromeService.isPalindrome(array3);
+        boolean isPalindrome4 = PalindromeService.isPalindrome(array4);
+        boolean isPalindrome5 = PalindromeService.isPalindrome(array5);
 
         System.out.println(isPalindrome1);
         System.out.println(isPalindrome2);
         System.out.println(isPalindrome3);
         System.out.println(isPalindrome4);
+        System.out.println(isPalindrome5);
     }
 }
--- src/main/java/pl/javastart/task/PalindromeService.java
+++ src/main/java/pl/javastart/task/PalindromeService.java
@@ -1,26 +1,18 @@
 package pl.javastart.task;
 
 public class PalindromeService {
-
-    // nie zmieniaj sygnatury tej metody. Jest ona testowana w PalindromeServiceTest
-    public boolean isPalindrome(int[] array) {
-
+    public static boolean isPalindrome(int[] array) {
         if (array == null || array.length == 0) {
             return false;
         }
 
-        int number = 0;
-        for (int i : array) {
-            number = number * 10 + i;
+        int length = array.length;
+        for (int i = 0; i < length / 2; i++) {
+            if (array[i] != array[length - i - 1]) {
+                return false;
+            }
         }
-
-        int originalNumber = number;
-
-        int reversedNumber = 0;
-        while (originalNumber != 0) {
-            reversedNumber = reversedNumber * 10 + originalNumber % 10;
-            originalNumber /= 10;
-        }
-        return number == reversedNumber;
+        return true;
     }
 }
+
